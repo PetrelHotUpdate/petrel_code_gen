b4d17fa2be6c7125ec51ba03266c6b7368e81e18
josercc <josercc@163.com>
2025-04-14 15:18:41 +0800

    [Change] 完成了基本生成register的builder代码

--- lib/src/pertrel_register_builder.dart
+++ lib/src/pertrel_register_builder.dart
@@ -11,67 +11,126 @@ class PetrelRegisterBuilder
   generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep) {
     final fileName = basename(buildStep.inputId.path);
-    if (element is ClassElement) {
-      final className = element.name;
-      final newClassName = className.replaceFirst('_', '');
+    if (element is! ClassElement) {
+      throw Exception('PetrelRegisterClass must be used on a class');
+    }
+    final className = element.name;
+    final newClassName = className.replaceFirst('_', '');
 
-      final methodParamStringList = <String>[];
-      final registerBuffer = StringBuffer();
-      final methodBuffer = StringBuffer();
+    final codeBuffer = StringBuffer();
+    final constructorParamsBuffer = StringBuffer();
+    final methodBuffer = StringBuffer();
+    final registerBuffer = StringBuffer();
 
-      // 生成构造函数和字段
-      final methodsWithAnnotation = <MethodElement>[];
-      for (var method in element.methods) {
-        if (!method.metadata
-            .any((e) => e.element?.displayName == 'PetrelRegisterMethod')) {
-          continue;
-        }
-        methodsWithAnnotation.add(method);
-        final methodType = method.returnType.getDisplayString();
-        final methodName = method.name;
-        final methodParams = method.parameters;
-        methodParamStringList
-            .add('required $methodType Function() $methodName,');
-        registerBuffer.writeln('''
-register('$methodName', (data) {
-      return $methodName();
+    // 生成构造函数和字段
+    final methodsWithAnnotation = element.methods.where((method) {
+      return method.metadata.any(
+        (e) => e.element?.displayName == 'PetrelRegisterMethod',
+      );
     });
-''');
 
-        methodBuffer.writeln('''
-@override
-$methodType $methodName(${methodParams.map((e) => e.type.getDisplayString()).join(', ')}) {
-  return nativeChannelEngine.call(CallMessageChannel(
-    '$methodName',
-    libraryName: libraryName,
-    className: className,
-    arguments: {},
-  ));
-}
-''');
-      }
+    for (final method in methodsWithAnnotation) {
+      final methodType = method.returnType.getDisplayString();
+      final methodName = method.name;
+      final methodParams = method.parameters;
 
-      // 生成构造函数
-      final paramList = methodParamStringList.join('');
-      String initMethod = '''
-$newClassName({$paramList}) {
-  ${registerBuffer.toString()}
-}
-''';
-      final codeBuffer = StringBuffer();
-      codeBuffer.write('''
+      constructorParamsBuffer.writeln('''
+        required $methodType Function(${_generateHandlerParams(methodParams)}) $methodName,
+      ''');
+      registerBuffer.writeln(_generateRegisterCode(methodName, methodParams));
+      methodBuffer.writeln(_generateMethodCode(method));
+    }
+
+    codeBuffer.writeln('''
 part of "$fileName";
 
 class $newClassName extends $className {
-  $initMethod
+  $newClassName({
+    ${constructorParamsBuffer.toString()}
+  }) {
+    ${registerBuffer.toString()}
+  }
 
   ${methodBuffer.toString()}
 }
 ''');
-      return codeBuffer.toString();
-    }
-    return null;
+    return codeBuffer.toString();
   }
 
-  String _generateMethodParams(List<ParameterElement> params) {}
+  String _generateHandlerParams(List<ParameterElement> params) {
+    StringBuffer buffer = StringBuffer();
+    List<String> positionalParams = [];
+    List<String> optionalParams = [];
+    List<String> optionalNamedParams = [];
+    for (final param in params) {
+      final displayString = param
+          .getDisplayString()
+          .replaceFirst('[', '')
+          .replaceFirst(']', '')
+          .replaceFirst('{', '')
+          .replaceFirst('}', '')
+          .replaceFirst('required', '')
+          .split('=')
+          .first;
+      final runtimeType = param.runtimeType.toString();
+      if (runtimeType == 'DefaultParameterElementImpl') {
+        if (param.isNamed) {
+          optionalNamedParams.add(displayString);
+        } else {
+          optionalParams.add(displayString);
+        }
+      } else {
+        positionalParams.add(displayString);
+      }
+    }
+    if (positionalParams.isNotEmpty) {
+      String positionalParamsString = positionalParams.join(', ');
+      buffer.write(positionalParamsString);
+      if (optionalParams.isNotEmpty || optionalNamedParams.isNotEmpty) {
+        buffer.write(',');
+      }
+    }
+    if (optionalParams.isNotEmpty) {
+      String optionalParamsString = optionalParams.join(', ');
+      buffer.write('[$optionalParamsString]');
+    }
+    if (optionalNamedParams.isNotEmpty) {
+      String optionalNamedParamsString = optionalNamedParams.join(', ');
+      buffer.write('{$optionalNamedParamsString}');
+    }
+
+    return buffer.toString();
+  }
+
+  String _generateRegisterCode(
+      String methodName, List<ParameterElement> params) {
+    StringBuffer buffer = StringBuffer();
+    for (final param in params) {
+      if (param.isNamed) {
+        buffer.write("${param.name}: channelData.data['${param.name}'],");
+      } else {
+        buffer.write("channelData.data['${param.name}'],");
+      }
+    }
+    return '''
+register('$methodName', (channelData) {
+      return $methodName(${buffer.toString()});
+});
+    ''';
+  }
+
+  String _generateMethodCode(MethodElement method) {
+    final methodName = method.name;
+    final params = method.parameters;
+    final displayString = method.getDisplayString();
+    print('displayString: $displayString');
+    return '''
+  @override
+  $displayString {
+    return call('$methodName', {
+      ${params.map((e) => "'${e.name}': ${e.name},").join('')}
+    });
+  }
+''';
+  }
 }
